f266080347e1218be459c2d2bd742b9893a58438
Bayan Bennett <bayan.bennett@gmail.com>
2021-01-11 20:46:46 +0200

    fix(styled-components): seal sheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,28 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class extends Document {
   static getInitialProps: typeof Document.getInitialProps = async (context) => {
     const sheet = new ServerStyleSheet();
-    const { renderPage: originalRenderPage } = context;
-    context.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
-      });
-    const initialProps = await Document.getInitialProps(context);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      )
+    const originalRenderPage = context.renderPage;
+    try {
+      context.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(context);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
     }
-  }
+  };
 }
